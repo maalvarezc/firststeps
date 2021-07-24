09616ea40edc614cd7c37286519a89c061ff8e06
Mario Alvarez <maac@python.com>
2021-07-24 00:11:28 -0500

    OK

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ def run():
     print(f'{nombre} tiene {edad} años')
     print('Se agrega una nueva línea')
     print('Otra línea')
+    print('Línea correcta')
 
 if  __name__ == '__main__':
     run()
